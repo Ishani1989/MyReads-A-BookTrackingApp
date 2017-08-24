22c453053319a5d9d3d6adb9331892335e666224
Ishani Mukherjee <ishanibhattacharjee@gmail.com>
2017-08-23 17:16:49 -0700

    replaced inline code for list books with ListBooks component

---
+++ src/SearchBooks.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import escapeRegExp from 'escape-string-regexp'
+
+class SearchBooks extends Component{
+    static propTypes = {
+        books : PropTypes.array
+    }
+
+    state = {
+        query : "",
+        maxResults : 5,
+        books : []
+    }
+
+    updateQuery = (query)=>{
+        this.setState({
+            query : query.trim(),
+            maxResults:5
+        })
+        if(query){
+        BooksAPI.search(this.state.query, this.state.maxResults).then((books) => {
+            this.setState({books: books})
+        })}
+        else{
+            this.setState({books: []})
+        }
+    }
+
+    clearQuery =()=>{
+        this.setState({
+            query : ""
+        })
+    }
+
+    render() {
+        const {query}=this.state.query
+        let closeSearch =   this.props.closeSearch;        
+    
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                <Link to='/' className='close-search'></Link>
+                <div className="search-books-input-wrapper">
+                    <input type="text" placeholder="Search by title or author" value={query}
+                        onChange={(event) => this.updateQuery(event.target.value)}/>
+                </div>
+                </div>
+                <div className="search-books-results">
+                    <div className="list-books">
+                        <div className="list-books-title">
+                            <h1>Your Search:</h1>
+                        </div>
+                        <div>
+                            {this.state.books ? (
+                                <ol className="books-grid">
+                                {this.state.books.map((book)=>(
+                                    <li key={book.id}>
+                                    {'imageLinks' in book ? (<div className='book-cover' style={{
+                                    width: 128, height: 188, backgroundImage: `url(${book.imageLinks.thumbnail})`
+                                    }}/>
+                                    ) : (<div className='book-cover' style={{
+                                             width: 128, height: 188, backgroundImage: `url("./icons/noimage.jpg")`
+                                            }}/>)
+                                    }
+                                        <div >
+                                            <select>
+                                            <option value="none" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                        <p>{book.title}</p>
+
+                                    </li>))}
+                                </ol>):(
+                                <div><h4>No results to show</h4></div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+         
+        )
+    }
+
+}
+
+
+export default SearchBooks
